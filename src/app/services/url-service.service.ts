83fb9787c7d32983a5a12c3cb64c912ff40c161b
zahariaca <zaharia.c.alexandru@gmail.com>
2020-06-11 19:14:47 +0300

    Added travis.yml initial build file

---
+++ src/app/services/url-service.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UrlService {
+
+  constructor() { }
+
+  todo(urlValue) {
+    console.log("UrlService-- " + urlValue)
+  }
+}
